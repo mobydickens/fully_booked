a120bf08bc3131d266376a3ff5a50e1080d3d4ff
mobydickens <steadmegan@gmail.com>
2019-03-19 14:17:55 -0600

    can change size of pictures individually

--- client_identifly/src/components/Photos.jsx
+++ client_identifly/src/components/Photos.jsx
@@ -3,21 +3,36 @@ import React, { Component } from 'react';
 class Photos extends Component {
 
   state = {
-    largePictureId: 0
+    largePictureId: 0,
+    currentPictureId: 0
   }
 
-  reduce = () => {
-    this.setState({ largePictureId: 0 })
+  changeSize = (id) => {
+    if(this.state.largePictureId === 0) {
+      this.setState({ 
+        largePictureId: this.props.sightingId,
+        currentPictureId: id
+      })
+    } else {
+      this.setState({ 
+        largePictureId: 0,
+        currentPictureId: 0
+      })
+    }
   }
 
   render() {
     let photoList = this.props.photos.map(photo => {
       return(
-        <div
-          onClick={()=>this.setState({ largePictureId: this.props.sightingId })}
-          key={photo.id}
-          className='ml-2' 
-          style={{cursor: 'pointer', width: 100, height: 100, backgroundImage: 'url('+ photo.photo_url +')', backgroundSize: 'cover' }}>
+        <div>
+          <div
+            onClick={()=>this.changeSize(photo.id)}
+            key={photo.id}
+            className='ml-2' 
+            style={ this.state.largePictureId === this.props.sightingId && this.state.currentPictureId === photo.id
+              ? {cursor: 'pointer', width: 500, height: 500, backgroundImage: 'url('+ photo.photo_url +')', backgroundSize: 'cover' }
+              : {cursor: 'pointer', width: 100, height: 100, backgroundImage: 'url('+ photo.photo_url +')', backgroundSize: 'cover' } }>
+          </div>
         </div>
       )
     })
